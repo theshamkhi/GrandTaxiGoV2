f48d5fa7c4155dfdcf9c6e870c0001e475da0fdd
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-03-06 13:49:18 +0000

    Added necessary routes for reviews

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AvailabilityController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\GoogleLoginController;
 use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ReviewController;
 
 Route::get('/', function () {
     return redirect()->route('dashboard');
@@ -18,14 +19,14 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
 
 // Trips
 Route::resource('trips', TripController::class)->middleware(['auth']);
+Route::patch('/trips/{trip}/update-status', [TripController::class, 'updateStatus'])->name('trips.update-status')->middleware('auth');
 
-Route::patch('/trips/{trip}/update-status', [TripController::class, 'updateStatus'])
-    ->name('trips.update-status')
-    ->middleware('auth');
+// Reviews
+Route::post('/trips/{trip}/reviews', [ReviewController::class, 'store'])->name('reviews.store');
+Route::delete('/reviews/{review}', [ReviewController::class, 'destroy'])->name('reviews.destroy');
 
 // Availability (for drivers)
-Route::resource('availability', AvailabilityController::class)
-    ->middleware(['auth', 'role:driver']);
+Route::resource('availability', AvailabilityController::class)->middleware(['auth', 'role:driver']);
 
 // Socialite Login
 Route::get('/auth/google', [GoogleLoginController::class, 'redirectToGoogle'])->name('auth.google');
